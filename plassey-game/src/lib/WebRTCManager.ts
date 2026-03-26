c8ecb56284d7b193e6bfd2ad154ca300ae433dbe
rakinthegreat <rakinthegreat@gmail.com>
2026-03-26 16:32:07 +0600

    phone 2

--- plassey-game/src/lib/WebRTCManager.ts
+++ plassey-game/src/lib/WebRTCManager.ts
@@ -7,7 +7,13 @@ const ICE_SERVERS = {
     { urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302'] },
     { urls: ['stun:openrelay.metered.ca:80'] },
     { 
-      urls: ['turn:openrelay.metered.ca:80', 'turn:openrelay.metered.ca:443'],
+      urls: [
+        'turn:openrelay.metered.ca:80?transport=udp',
+        'turn:openrelay.metered.ca:80?transport=tcp',
+        'turn:openrelay.metered.ca:443?transport=udp',
+        'turn:openrelay.metered.ca:443?transport=tcp',
+        'turns:openrelay.metered.ca:443?transport=tcp'
+      ],
       username: 'openrelayproject',
       credential: 'openrelayproject'
     }
